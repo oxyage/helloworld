3fa8cc512f5da97cadca4ee5d8b84576fe6061bd
oxyage <oxyage@yandex.ru>
2019-08-13 21:25:35 +0300

    Signed-off-by: oxyage <oxyage@yandex.ru>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+
+import static java.lang.StrictMath.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
+        int a = 81;
+        int q = 9;
+
         System.out.println("Hello World!");
         System.out.println("Пишу первое приложение на Java ");
         System.out.print(12);
         System.out.println(" месяцев в году");
+        System.out.println(sqrt(a/q)+" "+ 12);
     }
 }
